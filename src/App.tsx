d9014aace079c1d959a0cc890f829f0086484fd0
samclee <samclee@gmail.com>
2021-09-04 18:27:39 -0700

    Wee-hoo, got tabs in

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import { TrimmerTab, AboutTab } from './Tabs';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tabs>
+        <TabList>
+          <Tab>Trimmer</Tab>
+          <Tab>About</Tab>
+        </TabList>
+        <TabPanel>
+          <TrimmerTab />
+        </TabPanel>
+        <TabPanel>
+          <AboutTab />
+        </TabPanel>
+      </Tabs>
     </div>
   );
 }
